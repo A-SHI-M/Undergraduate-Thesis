41115beef05446aaead572648304dec37ce0abcb
A-SHI-M <1647dasashim1647@gmail.com>
2026-04-13 22:09:14 +0600

    requirements added

---
+++ README.md
@@ -0,0 +1,25 @@
+# Undergraduate-Thesis
+
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```bash
+https://github.com/A-SHI-M/Undergraduate-Thesis
+```
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -n paper python=3.10 -y
+```
+
+```bash
+conda activate paper
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Undergraduate-Thesis"
+AUTHOR_USER_NAME = "A-SHI-M"
+SRC_REPO = "Anomaly-Detection"
+AUTHOR_EMAIL = "1647dasashim1647@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for Undergraduate Thesis",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
